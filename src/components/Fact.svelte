dbb90b8e3164b94a4193b96b23ea72a4893aee62
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-07-23 13:51:34 -0600

    feat: fixed random facts not working with svelte component

---
+++ src/components/Fact.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	export let facts:string[];
+	const randomFact = facts[Math.floor(Math.random() * facts.length)];
+	// console.log(facts)
+</script>
+<p class="text-center text-white font-medium">{randomFact}</p>
